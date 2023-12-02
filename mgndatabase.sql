b26a19ea5a686dc1ca9789a8aeae68dfd0da8fd8
Frata23 <49278556+Frata23@users.noreply.github.com>
2023-12-02 11:36:35 +0100

    DataBase Update
    Ajout de la base de donnée `mgndatabase.sql`
    ainsi que son fichier de création `mgnCreationDataBase.sql`

---
+++ mgndatabase.sql
@@ -0,0 +1,246 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : sam. 02 déc. 2023 à 10:30
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `mgndatabase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE IF NOT EXISTS `client` (
+  `idClient` int NOT NULL AUTO_INCREMENT,
+  `nomClient` varchar(45) NOT NULL,
+  `prenomClient` varchar(45) NOT NULL,
+  `dateNaissance` date NOT NULL,
+  `estInscrit` tinyint(1) NOT NULL,
+  `numeroTelephone` int DEFAULT NULL,
+  `mail` varchar(45) DEFAULT NULL,
+  `adresse` varchar(45) DEFAULT NULL,
+  `codePostale` int DEFAULT NULL,
+  `profession` varchar(45) DEFAULT NULL,
+  `situation` varchar(45) DEFAULT NULL,
+  `revenuMensuel` decimal(15,2) DEFAULT NULL,
+  `montantDecouvert` int DEFAULT NULL,
+  PRIMARY KEY (`idClient`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `compte`
+--
+
+DROP TABLE IF EXISTS `compte`;
+CREATE TABLE IF NOT EXISTS `compte` (
+  `idCompte` int NOT NULL AUTO_INCREMENT,
+  `solde` int NOT NULL,
+  `type` varchar(45) NOT NULL,
+  PRIMARY KEY (`idCompte`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contracte`
+--
+
+DROP TABLE IF EXISTS `contracte`;
+CREATE TABLE IF NOT EXISTS `contracte` (
+  `idClient` int NOT NULL,
+  `idContrat` int NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `dateVente` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `contrat`
+--
+
+DROP TABLE IF EXISTS `contrat`;
+CREATE TABLE IF NOT EXISTS `contrat` (
+  `idContrat` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(45) NOT NULL,
+  PRIMARY KEY (`idContrat`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `creerclient`
+--
+
+DROP TABLE IF EXISTS `creerclient`;
+CREATE TABLE IF NOT EXISTS `creerclient` (
+  `idClient` int NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `dateCreation` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employe`
+--
+
+DROP TABLE IF EXISTS `employe`;
+CREATE TABLE IF NOT EXISTS `employe` (
+  `login` varchar(45) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `poste` varchar(45) NOT NULL,
+  `nomEmploye` varchar(45) NOT NULL,
+  `prenomEmploye` varchar(45) NOT NULL,
+  `dateEmbauche` date NOT NULL,
+  PRIMARY KEY (`login`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `horaireagence`
+--
+
+DROP TABLE IF EXISTS `horaireagence`;
+CREATE TABLE IF NOT EXISTS `horaireagence` (
+  `jour` varchar(45) NOT NULL,
+  `horaireOuverture` time NOT NULL,
+  `horaireFermeture` time NOT NULL,
+  PRIMARY KEY (`jour`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `modifiertype`
+--
+
+DROP TABLE IF EXISTS `modifiertype`;
+CREATE TABLE IF NOT EXISTS `modifiertype` (
+  `dateModificationType` date NOT NULL,
+  `login` varchar(45) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `piecetype`
+--
+
+DROP TABLE IF EXISTS `piecetype`;
+CREATE TABLE IF NOT EXISTS `piecetype` (
+  `type` varchar(45) NOT NULL,
+  `pieces` varchar(45) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `possedecompte`
+--
+
+DROP TABLE IF EXISTS `possedecompte`;
+CREATE TABLE IF NOT EXISTS `possedecompte` (
+  `idClient` int NOT NULL,
+  `idContrat` int NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `dateCreation` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rattachera`
+--
+
+DROP TABLE IF EXISTS `rattachera`;
+CREATE TABLE IF NOT EXISTS `rattachera` (
+  `idClient` int NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `dateRattachement` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rendezvous`
+--
+
+DROP TABLE IF EXISTS `rendezvous`;
+CREATE TABLE IF NOT EXISTS `rendezvous` (
+  `idRdv` int NOT NULL AUTO_INCREMENT,
+  `jourReunion` date NOT NULL,
+  `heureDebut` time NOT NULL,
+  `heureFin` time NOT NULL,
+  `dateCreationRdv` date NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `idClient` int NOT NULL,
+  `Motif` varchar(45) NOT NULL,
+  PRIMARY KEY (`idRdv`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type`
+--
+
+DROP TABLE IF EXISTS `type`;
+CREATE TABLE IF NOT EXISTS `type` (
+  `type` varchar(45) NOT NULL,
+  PRIMARY KEY (`type`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `typecompte`
+--
+
+DROP TABLE IF EXISTS `typecompte`;
+CREATE TABLE IF NOT EXISTS `typecompte` (
+  `typeCompte` varchar(45) NOT NULL,
+  `decouvert` int DEFAULT NULL,
+  `plafond` int DEFAULT NULL,
+  `interet` decimal(2,3) NOT NULL,
+  `propose` tinyint(1) NOT NULL,
+  PRIMARY KEY (`typeCompte`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `typecontrat`
+--
+
+DROP TABLE IF EXISTS `typecontrat`;
+CREATE TABLE IF NOT EXISTS `typecontrat` (
+  `typeContrat` varchar(45) NOT NULL,
+  PRIMARY KEY (`typeContrat`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
